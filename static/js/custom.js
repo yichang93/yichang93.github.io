5ad52e8bd3ba59d6cc64084819f26b3fc261d1a3
Li Yichang <ariellee121@gmail.com>
2020-01-06 11:45:22 +0800

    fix animation

--- static/js/custom.js
+++ static/js/custom.js
@@ -117,14 +117,13 @@
 		/* Check the location of each desired element */
 		$('.hideme').each( function(i){
 			
-			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+			var bottom_of_object = $(this).offset().top + $(this).outerHeight()/2;
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
 			
 			/* If the object is completely visible in the window, fade it it */
 			if( bottom_of_window > bottom_of_object ){
+				$(this).addClass('slide-top')
 				
-				$(this).animate({'opacity':'1'},500);
-					
 			}
 			
 		}); 
@@ -222,12 +221,6 @@
             });
         }
 	}
-	
-	NAY.scrollHint = function() {
-		if ($("#scroll-hint").exists()){
-			$("#scroll-hint").addClass("slide-bottom")
-		}
-	}
 
 	
 
@@ -269,7 +262,6 @@
 		NAY.Gallery(),
 		NAY.HeaderHeight(),
 		NAY.mTypeIt(),
-		NAY.scrollHint();
 		NAY.FadeIn();
 	});
 
