5033200442bd22412467f4674b35c9813311749d
Li Yichang <ariellee121@gmail.com>
2020-01-07 14:21:45 +0800

    change theme color

--- static/js/custom.js
+++ static/js/custom.js
@@ -116,8 +116,8 @@
 			
 		/* Check the location of each desired element */
 		$('.hideme').each( function(i){
-			
-			var bottom_of_object = $(this).offset().top + $(this).outerHeight()/2;
+			var benchmark = $(this).outerHeight()/2 > 150 ? 150 : $(this).outerHeight()/2;
+			var bottom_of_object = $(this).offset().top + benchmark;
 			var bottom_of_window = $(window).scrollTop() + $(window).height();
 			
 			/* If the object is completely visible in the window, fade it it */
